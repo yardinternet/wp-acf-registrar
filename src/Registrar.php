a6ede49d1509e54dc1ac14613995f2893e013b8b
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2025-09-29 17:03:28 +0200

    feat: add form settings

--- src/Registrar.php
+++ src/Registrar.php
@@ -40,4 +40,46 @@ class Registrar
 			register_extended_field_group($settings);
 		}
 	}
+
+	public function registerForms(): void
+	{
+		$forms = config('acf-registrar.forms', []);
+
+		foreach ($forms as $form) {
+			if (! is_a($form, Form::class, true)) {
+				throw new \RuntimeException(sprintf('The class "%s" must extend %s.', $form, Form::class));
+			}
+
+			/** @var Form $form */
+			$form = new $form();
+
+			$settings = [
+				'id' => $form->getID(),
+				'post_id' => $form->getPostID(),
+				'new_post' => $form->getNewPost(),
+				'field_groups' => $form->getFieldGroups(),
+				'fields' => $form->getFields(),
+				'post_title' => $form->getPostTitle(),
+				'post_content' => $form->getPostContent(),
+				'form' => $form->getForm(),
+				'form_attributes' => $form->getFormAtrributes(),
+				'return' => $form->getReturn(),
+				'html_before_fields' => $form->getHtmlBeforeFields(),
+				'html_after_fields' => $form->getHtmlAfterFields(),
+				'submit_value' => $form->getSubmitValue(),
+				'updated_message' => $form->getUpdatedMessage(),
+				'label_placement' => $form->getLabelPlacement(),
+				'instruction_placement' => $form->getInstructionPlacement(),
+				'field_el' => $form->getFieldEl(),
+				'uploader' => $form->getUploader(),
+				'honeypot' => $form->getHoneypot(),
+				'html_update_message' => $form->getHtmlUpdatedMessage(),
+				'html_submit_button' => $form->getHtmlSubmitButton(),
+				'html_submit_spinner' => $form->getHtmlSubmitSpinner(),
+				'kses' => $form->getKses(),
+			];
+
+			acf_register_form($settings);
+		}
+	}
 }
